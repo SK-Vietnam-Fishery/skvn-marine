1f483b6d7cb7650314906b15ebd1d9c79a49e7bd
ITSKVN <sk.it@skvngroup.com>
2026-06-17 14:29:23 +0700

    Update build tools

--- wp-content/plugins/skvn-marine-blocks/build/index.ts.asset.php
+++ wp-content/plugins/skvn-marine-blocks/build/index.ts.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-editor', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-notices', 'wp-plugins'), 'version' => 'd2e0425005b55a2740ae');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-editor', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-notices', 'wp-plugins'), 'version' => '39b156eaf22229644577');
